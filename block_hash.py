332b63b33bf3fa82f5c377b78e6f425da20043af
1337tester <miso.zima@gmail.com>
2017-06-28 17:27:22 +0200

    Add description into block_hash.py

--- block_hash.py
+++ block_hash.py
@@ -1,3 +1,15 @@
+"""
+Implementation of a SHA256 for hashing and verification in 1 Kb blocks. This enables possible launch of verified blocks before the whole file is verified - basis for streaming for example.
+
+Installation:
+Copy the 'block_hash.py' to your computer
+launch it with 'python3 block_hash.py' (you must have python 3.x installed)
+Follow instructions
+
+Disclaimer:
+this program was never meant for serious use, I wanted a MVP for my own training, use therefore with caution
+provides only resistance against random data loss by design, does not protect against existential forgery
+"""
 from Crypto.Hash import SHA256
 
 
